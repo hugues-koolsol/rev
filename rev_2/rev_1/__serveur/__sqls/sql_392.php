12eb7b8cb98c4332ac96c3654fcbbd5e0448fedf
hugues-koolsol <hugues@koolsol.com>
2025-12-23 16:49:38 +0100

    2025-12-23-a

---
+++ rev_2/rev_1/__serveur/__sqls/sql_392.php
@@ -0,0 +1,24 @@
+<?php
+function sql_392($par,&$donnees_retournees,$that,$db){
+    $sql0='
+      UPDATE tbl_projets SET 
+                  `chi_id_projet` = '.sq1($par['n_chi_id_projet']).' , 
+                  `chp_nom_projet` = '.sq1($par['n_chp_nom_projet']).'
+                WHERE `chi_id_projet` = '.sq1($par['c_chi_id_projet']).' ;
+    ';
+    // echo __FILE__ . ' ' . __LINE__ . ' $sql0 = <pre>' . $sql0 . '</pre>' ; exit(0);
+    try{
+        $ret=$db->exec($sql0);
+        return(array( __xst => __xsu ));
+    }catch(Exception $e){
+        return(
+            array( 
+                __xst => __xer, 
+                'exception' => $e , 
+                'sql0'    => $sql0 ,
+                'code_erreur' => $db->lastErrorCode() ,
+                __xme => 'erreur sql_392()'.' '.$db->lastErrorMsg(),
+            )
+        );
+    }
+}
